d934912b2f9d14936988d5f1de6a721f17f0a571
Tarunchintakunta <tarunchintakunta@gmail.com>
2026-02-01 01:03:47 +0530

    chore(features): strip comments in cart and payments

--- src/features/cart/hooks/use-cart.ts
+++ src/features/cart/hooks/use-cart.ts
@@ -1,20 +1,15 @@
 "use client";
-
 import { useCart as useCartQuery, useAddToCart as useAddToCartMutation, useRemoveFromCart as useRemoveFromCartMutation, useClearCart as useClearCartMutation } from "@/lib/hooks/use-data";
-
 export const useCart = () => useCartQuery();
-
 export const useAddToCart = () => {
-  const mutation = useAddToCartMutation();
-  return mutation;
+    const mutation = useAddToCartMutation();
+    return mutation;
 };
-
 export const useRemoveFromCart = () => {
-  const mutation = useRemoveFromCartMutation();
-  return mutation;
+    const mutation = useRemoveFromCartMutation();
+    return mutation;
 };
-
 export const useClearCart = () => {
-  const mutation = useClearCartMutation();
-  return mutation;
+    const mutation = useClearCartMutation();
+    return mutation;
 };
